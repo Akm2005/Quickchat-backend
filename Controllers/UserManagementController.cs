a21d3b779ef3242295e9fe900c756f2cc122b521
Aman Mishra <12defence2005@gmail.com>
2025-03-06 15:48:42 +0530

    Add project files.

---
+++ Controllers/UserManagementController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Quickchat.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserManagementController : ControllerBase
+    {
+        [HttpPost("UserRegistration")]
+        public IActionResult Echo([FromBody] object body)
+        {
+            Console.WriteLine("Received Body: " + System.Text.Json.JsonSerializer.Serialize(body)); // Console me print karega
+            return Ok(body); // Jo body aayi, wahi return karega
+        }
+    }
+}
